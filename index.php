c5f99b2115521fca94a849b206fe1d226efd54d0
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-12 08:23:39 -0700

    add index.php and p tags.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Snap Coding Challenge</title>
+	</head>
+	<body>
+		<p>&iquest;Qui&eacuten</p> fue hnos de los poetas m&aacutes importantes en la historia de M&eacuteico? Sor Juana In&eacutes de la Cruz. Uno de sus eleg&iacuteas m&aacutes importantes fue &<<De la beldad de Laura enamorados>>.
+
+		Who was one of the most important poets in the history of Mexico? Sor Juana In&eacutes de la Cruz. One of her most important selections was <<De la beldad de Laura enamorados>>.
+	</body>
+</html>
