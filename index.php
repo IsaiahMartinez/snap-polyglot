d681e6e6845601c0bc3552858f495a032c5bb010
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-12 08:29:06 -0700

    added &lt, &gt symbols

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 		<title>Snap Coding Challenge</title>
 	</head>
 	<body>
-		<p>&iquest;Qui&eacuten</p> fue hnos de los poetas m&aacutes importantes en la historia de M&eacuteico? Sor Juana In&eacutes de la Cruz. Uno de sus eleg&iacuteas m&aacutes importantes fue &<<De la beldad de Laura enamorados>>.
+		<p>&iquest;Qui&eacuten fue unos de los poetas m&aacutes importantes en la historia de M&eacutexico? Sor Juana In&eacutes de la Cruz. Uno de sus eleg&iacuteas m&aacutes importantes fue &lt;&lt; De la beldad de Laura enamorados.&gt;&gt;</gt></p>
 
-		Who was one of the most important poets in the history of Mexico? Sor Juana In&eacutes de la Cruz. One of her most important selections was <<De la beldad de Laura enamorados>>.
+		Who was one of the most important poets in the history of Mexico? Sor Juana In&eacutes de la Cruz. One of her most important selections was &lt;&lt; De la beldad de Laura enamorados.&gt;&gt;
 	</body>
 </html>
